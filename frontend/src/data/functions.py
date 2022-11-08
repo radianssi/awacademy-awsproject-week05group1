93466b145fd63efd3695abd851285bdaed71b7ad
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-11-08 16:29:35 +0200

    Fixed datatimes from inster_data

--- frontend/src/data/functions.py
+++ frontend/src/data/functions.py
@@ -6,8 +6,8 @@ def insert_data():
     pass
 
     # date = "08.11.2022"
-    # y = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # date2 = datetime.datetime.strptime(date, "%d.%m.%Y")
+    # y = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # date2 = datetime.strptime(date, "%d.%m.%Y")
     # print(date2)
 
 def push_data_to_database(start_time, end_time, work_hours, project, project_desc):
