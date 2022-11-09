5264a0ae7ffdefc7cc35325240c66ec3abf2dc85
Santeri <santeri.nenonen@brightstraining.com>
2022-11-09 10:52:41 +0200

    Initialized main.py to run functions

--- frontend/src/data/main.py
+++ frontend/src/data/main.py
@@ -1,7 +1,19 @@
 from functions import insert_data, push_data_to_database
 
 def main():
-    data = insert_data()    
+    print("Welcome to Time Managament Tool!")
+    print("This tool allows you to insert data into the Time Managament database.")
 
-    def push_data_to_database(data[0], data[1]):
-        pass
+    while True:
+        run_functions = input("Do you wanto to add worked hours?(Y/N): ")
+        if run_functions == "y" or "Y":
+            data = insert_data()    
+            push_data_to_database(data[0], data[1])
+        elif run_functions == "n" or "N":
+            print("Thank you for using Time Managament app!")
+            break
+        else:
+            run_functions = input("Please select correct option: ")
+
+if __name__ == "__main__":
+    main()
