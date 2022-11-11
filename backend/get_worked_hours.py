8a96febc5e9ff978761c83cfe5529022145c0e22
Santeri <santeri.nenonen@brightstraining.com>
2022-11-11 13:06:59 +0200

    Finished backend function to query database

--- backend/get_worked_hours.py
+++ backend/get_worked_hours.py
@@ -1,37 +1,26 @@
 import psycopg2
 from config import config
-import pandas as pd
+import json
 
 
 
 
-def read_db():
-    con = None
-    SQL = 'select date(start_time), sum(work_hours) as hours_worked from timetable group by date(start_time) order by date(start_time) desc LIMIT 1;'
-    try:
-        con = psycopg2.connect(**config())
-        dataframe = pd.read_sql(SQL, con)   
-        print(dataframe)
+con = psycopg2.connect(**config())
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if con is not None:
-            con.close()
-
-
-
-
-def get_worked_hours():
+def get_db():
     con = None
     try:
         con = psycopg2.connect(**config())
         cursor = con.cursor()
-        SQL = 'select date(start_time), sum(work_hours) as hours_worked from timetable group by date(start_time) order by date(start_time) desc limit 1;'
+        SQL = '''select * from timetable where start_time > (now() - interval '24 hours');'''
+        SQL2 = '''select date(start_time), sum(work_hours) as hours_worked from timetable group by date(start_time) order by date(start_time) desc limit 1;'''
         cursor.execute(SQL)
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+        response = cursor.fetchall()
+        data = json.dumps(response, default = str)
+        cursor.execute(SQL2)
+        response = cursor.fetchall()
+        data2 = json.dumps(response, default = str)
+        return [data, data2]
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -41,5 +30,5 @@ def get_worked_hours():
 
 
 if __name__ == '__main__':
-    read_db()
+    get_db()
 
